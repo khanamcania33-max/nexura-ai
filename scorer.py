133124bac4208855417562ce1072d5cae9296902
khanamcania33-max <khanamcania33@gmail.com>
2026-03-27 23:34:28 +0600

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -1,14 +1,39 @@
-def calculate_score(product):
-    demand = product["demandScore"]
-    margin = product["marginScore"]
-    competition = 100 - product["competitionScore"]
-    trend = product["trendScore"]
+def calculate_score(p):
 
-    score = (
-        demand * 0.35 +
-        margin * 0.25 +
-        competition * 0.20 +
-        trend * 0.20
-    )
+    score = 0
 
-    return round(score, 2)
+    # Demand
+    if p["searchVolume"] >= 3000:
+        score += 25
+    elif p["searchVolume"] >= 2000:
+        score += 20
+    else:
+        score += 10
+
+    # Competition
+    if p["reviews"] < 500:
+        score += 20
+    elif p["reviews"] < 1500:
+        score += 10
+
+    # Margin
+    if p["margin"] >= 30:
+        score += 25
+    elif p["margin"] >= 20:
+        score += 15
+    elif p["margin"] >= 15:
+        score += 10
+
+    # Weight
+    if p["weight"] <= 5:
+        score += 10
+
+    # Price rule
+    if p["price"] <= 40:
+        score += 10
+
+    # Evergreen
+    if p["trend"] == "Evergreen":
+        score += 10
+
+    return score
