c4d19bd17166badb5acc3d3743b66a825a8ad8f2
khanamcania33-max <khanamcania33@gmail.com>
2026-03-27 23:44:12 +0600

    scorer.py

--- scorer.py
+++ scorer.py
@@ -1,22 +1,16 @@
 def calculate_score(p):
-
     score = 0
 
-    # Demand
     if p["searchVolume"] >= 3000:
         score += 25
     elif p["searchVolume"] >= 2000:
         score += 20
-    else:
-        score += 10
 
-    # Competition
     if p["reviews"] < 500:
         score += 20
     elif p["reviews"] < 1500:
         score += 10
 
-    # Margin
     if p["margin"] >= 30:
         score += 25
     elif p["margin"] >= 20:
@@ -24,15 +18,12 @@ def calculate_score(p):
     elif p["margin"] >= 15:
         score += 10
 
-    # Weight
     if p["weight"] <= 5:
         score += 10
 
-    # Price rule
     if p["price"] <= 40:
         score += 10
 
-    # Evergreen
     if p["trend"] == "Evergreen":
         score += 10
 
